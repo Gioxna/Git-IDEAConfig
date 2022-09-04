6fb833c66acc34817ea3b4a8649e8509703ad1e9
Gioxna <Gioxna@163.com>
2022-09-04 15:26:10 +0800

    master Commit

--- src/main/java/com/gao/git/GitTest.java
+++ src/main/java/com/gao/git/GitTest.java
@@ -2,7 +2,7 @@ package com.gao.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello Git!");
+        System.out.println("hello Git1!");
         System.out.println("hello Git2!");
         System.out.println("hello Git3!");
     }
